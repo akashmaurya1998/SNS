279b04a0e5ca3eca0a3d31aec174342fd00283f5
aakash <akashmaurya400@gmail.com>
2021-08-12 18:12:26 +0530

    Before adding welcome Activity

--- app/src/main/java/com/aakash/sns/MainActivity.java
+++ app/src/main/java/com/aakash/sns/MainActivity.java
@@ -6,11 +6,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -19,10 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -70,27 +66,15 @@ public class MainActivity extends AppCompatActivity {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 //        SIGNING UP
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               signUp();
-            }
-        });
+        signUpBtn.setOnClickListener(v -> signUp());
 
 //        SIGNING IN
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
-        });
+        signInBtn.setOnClickListener(v -> signIn());
 
-        googleSignInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, RC_SIGN_IN);
-            }
+        googleSignInBtn.setOnClickListener(v -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
         });
     }
 
@@ -116,35 +100,22 @@ public class MainActivity extends AppCompatActivity {
         } else {
             signUpBtn.startAnimation();
             mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
 
-                                user = mAuth.getCurrentUser();
-                                UserProfileChangeRequest changeRequest = new UserProfileChangeRequest.Builder()
-                                        .setDisplayName(username)
-                                        .build();
+                            user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest changeRequest = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(username)
+                                    .build();
 
-                                user.updateProfile(changeRequest)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                Toast.makeText(MainActivity.this, "Welcome" + username, Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                            } else {
-                                Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                            signUpBtn.doneLoadingAnimation(android.R.color.white, bitmap);
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    signUpBtn.revertAnimation();
-                                }
-                            }, 1000);
-
+                            user.updateProfile(changeRequest)
+                                    .addOnCompleteListener(task1 -> Toast.makeText(MainActivity.this, "Welcome" + username, Toast.LENGTH_SHORT).show());
+                        } else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
+                        signUpBtn.doneLoadingAnimation(android.R.color.white, bitmap);
+                        new Handler().postDelayed(() -> signUpBtn.revertAnimation(), 1000);
+
                     });
         }
     }
@@ -166,30 +137,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             signInBtn.startAnimation();
             mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                user = mAuth.getCurrentUser();
-                                Toast.makeText(MainActivity.this, "Welcome " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
-                                signInBtn.doneLoadingAnimation(android.R.color.white, bitmap);
-                                new Handler().postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        signInBtn.revertAnimation();
-                                    }
-                                }, 1000);
-                            } else {
-                                Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                signInBtn.doneLoadingAnimation(android.R.color.white, bitmap);
-                                new Handler().postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        signInBtn.revertAnimation();
-                                    }
-                                }, 1000);
-                            }
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Welcome " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
+
+                        signInBtn.doneLoadingAnimation(android.R.color.white, bitmap);
+                        new Handler().postDelayed(() -> signInBtn.revertAnimation(), 1000);
                     });
         }
     }
@@ -204,11 +161,9 @@ public class MainActivity extends AppCompatActivity {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-//                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-//                Log.w(TAG, "Google sign in failed", e);
+                e.printStackTrace();
             }
         }
     }
@@ -216,21 +171,14 @@ public class MainActivity extends AppCompatActivity {
     private void firebaseAuthWithGoogle(String idToken) {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
         mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "signInWithCredential:success");
-                             user = mAuth.getCurrentUser();
-                            Toast.makeText(MainActivity.this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
-//                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-//                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-//                            updateUI(null);
-                            Toast.makeText(MainActivity.this, "Something went wrong!!", Toast.LENGTH_SHORT).show();
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                         user = mAuth.getCurrentUser();
+                        Toast.makeText(MainActivity.this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(MainActivity.this, "Something went wrong!!", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
